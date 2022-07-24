51595fb63c92861eb5849981391606a541512caf
KobayashiHanaka <konahana8787@gmail.com>
2022-07-24 09:53:59 +0000

    fix-usershow-and-user-index

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -24,7 +24,7 @@ class Public::UsersController < ApplicationController
 
   #いいね一覧
   def favorites
-    @user = User.find(params[:id])
+    @user = User.find_by(params[:user_id])
     favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
     @favorite_posts = Post.find(favorites)
   end
